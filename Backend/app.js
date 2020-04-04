0dbc9a57b770399263f60886a0646d28fe48ddfd
unknown <andrelopessfla@gmail.com>
2020-04-03 21:49:23 -0300

    configuração do mongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,27 @@
 const express = require('express')
-
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./src/config/db")
+
 
 
 
 //config
 
+//cofig do mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(db.mongoURI).then(()=>{
+    console.log("conectado ao mongoDB!")
+}).catch((err)=>{
+    console.log("erro ao de conexão: "+err)
+})
 
 //rotas
 app.get("/",(req,res)=>{
     res.send("rota principal")
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log("servidor rodando!")
---
+++ Backend/src/config/db.js
@@ -0,0 +1,10 @@
+if(process.env.NODE_ENV == "production"){
+    module.exports={mongoURI:""
+
+    }
+}else{
+    module.exports={mongoURI:"mongodb://localhost/blogapp"
+
+}
+
+}
